f466f06a29074d2179d0dc1ce30dd4dcb92eb6bf
nekalave <nekalave@gmail.com>
2024-06-16 18:59:38 +0300

    react-hw-02 complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
+import { Routes, Route } from 'react-router-dom';
+import Header from './Header/Header';
+import FeedbackPage from './FeedbackPage/FeedbackPage';
+import { Fragment } from 'react';
+import Home from './Home/Home';
+import PhonebookPage from './PhonebookPage/PhonebookPage';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Fragment>
+      <Header />
+      <Routes>
+        <Route path='/goit-react-hw-02' element={<Home />} />
+        <Route path='/feedback' element={<FeedbackPage />} />
+        <Route path='/phonebook' element={<PhonebookPage />} />
+      </Routes>
+    </Fragment>
   );
 };
+
+export default App;
